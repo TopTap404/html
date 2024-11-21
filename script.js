bf266efdfe421a4b24ccd88b94d515edc07833b8
BuildTools <unconfigured@null.spigotmc.org>
2024-11-21 18:43:20 +0700

    Add existing file

---
+++ script.js
@@ -0,0 +1,23 @@
+async function fetchTemperature() {
+    const displayElement = document.getElementById('temperatureDisplay');
+    try {
+      const response = await fetch('http://104.214.177.30:3001/getTemperature');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      const temperature = data.temperature;
+  
+      displayElement.textContent = temperature !== null
+        ? `Current Temperature: ${temperature}°C`
+        : "No temperature data available.";
+    } catch (error) {
+      console.error('Error fetching temperature:', error);
+      displayElement.textContent = "Failed to fetch temperature data.";
+    }
+  }
+  
+  document.getElementById('refreshButton').addEventListener('click', fetchTemperature);
+  
+  window.onload = fetchTemperature;
